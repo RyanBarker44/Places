3cfbbbd9e42a0fbf2f37f4ef8049ed7c2e993b58
Ryan <ryan.barker@griffithuni.edu.au>
2019-04-30 15:43:07 +1000

    Initial Commit

---
+++ PlacesAssignmentTests/PlacesAssignmentTests.swift
@@ -0,0 +1,34 @@
+//
+//  PlacesAssignmentTests.swift
+//  PlacesAssignmentTests
+//
+//  Created by Ryan Barker on 30/4/19.
+//  Copyright © 2019 Ryan Barker. All rights reserved.
+//
+
+import XCTest
+@testable import PlacesAssignment
+
+class PlacesAssignmentTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
